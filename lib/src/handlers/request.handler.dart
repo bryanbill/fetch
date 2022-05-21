98be9bebd64b065918144933a05aa82a60988b11
bryanbill <emobrya@gmail.com>
2022-05-21 13:12:23 +0300

    doc:Documentation - Documents RequestHandler methods

--- lib/src/handlers/request.handler.dart
+++ lib/src/handlers/request.handler.dart
@@ -10,6 +10,14 @@ class RequestHandler {
 
   RequestHandler(this.url, this.body, this.headers);
 
+  /// Returns a [Response] object with the response from the server.
+  ///
+  /// If the request fails, the [Response] object will have a null [Response.body]
+  /// and a non-null [Response.statusCode].
+  /// Example:
+  /// ```dart
+  /// Response response = await RequestHandler('https://www.google.com').get();
+  /// ```
   Future<Response> get() async {
     var request = await HttpClient().getUrl(Uri.parse(url));
     var response = await request.close();
@@ -17,6 +25,15 @@ class RequestHandler {
         response, await response.transform(Utf8Decoder()).join());
   }
 
+  /// Returns a [Response] object with the response from the server.
+  ///
+  /// If the request fails, the [Response] object will have a null [Response.body]
+  /// and [Response.statusCode] will be set to the status code of the error.
+  /// Example:
+  /// ```dart
+  /// Response response = await await RequestHandler('https://example.com/api/v1/users',
+  ///                        {'name': 'John Doe'}).post();
+  /// ```
   Future<Response> post() async {
     var request = await HttpClient().postUrl(Uri.parse(url));
     if (body != null) {
@@ -33,6 +50,15 @@ class RequestHandler {
         response, await response.transform(Utf8Decoder()).join());
   }
 
+  /// Returns a [Response] object with the response from the server.
+  ///
+  /// If the request fails, the [Response] object will have a null [Response.body]
+  /// Example:
+  /// ```dart
+  /// final response = await RequestHandler('https://example.com/api/v1/users/1',
+  ///                        {'name': 'John Doe'})
+  ///                        .put();
+  /// ```
   Future<Response> put() async {
     var request = await HttpClient().putUrl(Uri.parse(url));
     if (body != null) {
@@ -49,6 +75,14 @@ class RequestHandler {
         response, await response.transform(Utf8Decoder()).join());
   }
 
+  /// Returns a [Response] object with the response from the server.
+  ///
+  /// If the request fails, the [Response] object will have a null [Response.body]
+  /// /// Example:
+  /// ```dart
+  /// final response = await RequestHandler('https://example.com/api/v1/users/1')
+  ///                        .delete();
+  /// ```
   Future<Response> delete() async {
     var request = await HttpClient().deleteUrl(Uri.parse(url));
     if (body != null) {
@@ -65,6 +99,15 @@ class RequestHandler {
         response, await response.transform(Utf8Decoder()).join());
   }
 
+  /// Returns a [Response] object with the response from the server.
+  ///
+  /// If the request fails, the [Response] object will have a null [Response.body]
+  /// Example:
+  /// ```dart
+  /// final response = await RequestHandler('https://example.com/api/v1/users/1',
+  ///                        {'name': 'John Doe'})
+  ///                        .patch();
+  /// ```
   Future<Response> patch() async {
     var request = await HttpClient().patchUrl(Uri.parse(url));
     if (body != null) {
