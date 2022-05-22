d5dba7234a13bdb65eb6cd3333dcf210ad9d5735
bryanbill <emobrya@gmail.com>
2022-05-22 11:21:39 +0300

    doc:Documentation - Release documentation for v0.0.3

--- example/delete.dart
+++ example/delete.dart
@@ -1,9 +1,7 @@
 import 'package:fetchx/fetchx.dart';
 
-
 void main() async {
-
-    Response response = await 'https://jsonplaceholder.typicode.com/posts/1'.delete();
+  Response response =
+      await 'https://jsonplaceholder.typicode.com/posts/1'.delete();
   print(response.body);
-
 }
