b23005fc850d137ae101a09f6d906a47400296f6
bryanbill <emobrya@gmail.com>
2022-06-04 19:26:38 +0300

    ft:API - Exposes .to<T>() overload from Future

--- lib/src/fetch_base.dart
+++ lib/src/fetch_base.dart
@@ -1,3 +1,5 @@
 export './extensions/string.extension.dart';
 export './extensions/list.extension.dart';
+export './extensions/future.extension.dart';
 export './models/response.model.dart';
+export './models/base.model.dart';
